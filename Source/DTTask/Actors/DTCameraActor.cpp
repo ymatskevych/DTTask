63db125035d768be6fd7ec6b2c00f3988ae757ec
ymatskevych <matskevych.yurii@gmail.com>
2023-01-13 00:19:41 +0200

    Move camera logic to CPP

---
+++ Source/DTTask/Actors/DTCameraActor.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DTCameraActor.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void ADTCameraActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const UWorld* World = GetWorld();
+	if (!ensureMsgf(IsValid(World), TEXT("[ADTCameraActor]: World is invalid")))
+	{
+		return;
+	}
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(World, 0);
+	if (!ensureMsgf(IsValid(PlayerController), TEXT("[ADTCameraActor]: PlayerController is invalid")))
+	{
+		return;
+	}
+	PlayerController->SetViewTarget(this);
+}
---
+++ Source/DTTask/Actors/DTCameraActor.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "DTCameraActor.generated.h"
+
+UCLASS()
+class DTTASK_API ADTCameraActor : public ACameraActor
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void BeginPlay() override;
+};
